e20a002c30ce94a2a375f094b65011f9abd8e149
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-04-17 12:03:15 -0300

    exercicio 14 adicionado MEDIA ESCOLAR

--- Aula 14 Condicionais/src/ex14_aprovadoReprovado.java
+++ Aula 14 Condicionais/src/ex14_aprovadoReprovado.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+import javax.swing.DefaultBoundedRangeModel;
+
 public class ex14_aprovadoReprovado {
     public static void main(String[] args) {
-        System.out.println("Aprovado ou reprovado");
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("\nDigite a 1a. nota: ");
+        double nota1 = scan.nextDouble();
+        System.out.print("Digite a 2a. nota: ");
+        double nota2 = scan.nextDouble();
+        
+        String resultado, situacao;
+        double media = ( nota1 + nota2 ) / 2;
+        if (media >= 9) {
+            resultado = "A";
+            situacao = "Aprovado com louvor, ";
+        } else if (media >= 7.5) {
+            resultado = "B";
+            situacao = "Aprovado";
+        } else if (media >= 6) {
+            resultado = "C";
+            situacao = "Aprovado";
+        } else if (media >= 4 ) {
+            resultado = "D";
+            situacao = "Reprovado";
+        } else {
+            resultado = "E";
+            situacao = "Reprovado";
+        }
+
+        System.out.println("Sua media foi " + media);
+        System.out.println("Você foi " + situacao + " com nota " + resultado);
+        scan.close();
     }
 }
