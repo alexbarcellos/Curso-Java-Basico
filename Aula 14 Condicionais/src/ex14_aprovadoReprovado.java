0a78a1099d6df22c46ae09de76208b82ce1bf43e
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-04-17 12:07:45 -0300

    removido imports do ex.14

--- Aula 14 Condicionais/src/ex14_aprovadoReprovado.java
+++ Aula 14 Condicionais/src/ex14_aprovadoReprovado.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;
 
-import javax.lang.model.util.ElementScanner14;
-import javax.swing.DefaultBoundedRangeModel;
-
 public class ex14_aprovadoReprovado {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
