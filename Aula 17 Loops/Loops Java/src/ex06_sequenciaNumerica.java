85dbfdb2e48881d88157d5fb4c048870c7b7cf1b
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-04-30 19:52:33 -0300

    Correção do ex06_sequenciaNumerica em andamento

---
+++ Aula 17 Loops/Loops Java/src/ex06_sequenciaNumerica.java
@@ -0,0 +1,7 @@
+public class ex06_sequenciaNumerica {
+    public static void main(String [] args) {
+        for (int i =0 ; i <= 20 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
