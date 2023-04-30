5bc0fa99b620babe8c0e8decbe31fbee8e43f5a1
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-04-30 20:00:59 -0300

    ex06 sequencia numérica finalizado

--- Aula 17 Loops/Loops Java/src/ex06_sequenciaNumerica.java
+++ Aula 17 Loops/Loops Java/src/ex06_sequenciaNumerica.java
@@ -1,7 +1,10 @@
 public class ex06_sequenciaNumerica {
     public static void main(String [] args) {
-        for (int i =0 ; i <= 20 ; i++) {
-            System.out.println(i);
+        for (int i = 1 ; i <= 20 ; i++) {
+            System.out.println(i); // sequencia um abaico do outro
+        }
+        for (int i = 1 ; i <= 20 ; i++) {
+            System.out.print(i + " ");  // sequencia um ao lado do outro
         }
     }
 }
