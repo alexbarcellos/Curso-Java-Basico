b6d560950fe08b2c7bfcfa1fb658bb134161470f
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-05-04 14:30:53 -0300

    ex30 da tabuada personalizada melhorado

--- Aula 17 Loops/Loops Java/src/ex30_tabuadaPersonalizada.java
+++ Aula 17 Loops/Loops Java/src/ex30_tabuadaPersonalizada.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class ex30_tabuadaPersonalizada {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+
         System.out.print("\nTabuada do: ");
         int n = scan.nextInt();
+        int primeiro = 1;
+        int ultimo = 0;
 
-        System.out.print("multiplo inicial: ");
-        int primeiro = scan.nextInt();
+        while (primeiro > ultimo) {
+            System.out.print("multiplo inicial: ");
+            primeiro = scan.nextInt();
 
-        System.out.print("multiplo final: ");
-        int ultimo = scan.nextInt();
+            System.out.print("multiplo final: ");
+            ultimo = scan.nextInt();
+        }
 
         for ( int i = primeiro ; i <= ultimo ; i++ ) {
             int tab = n * i;
