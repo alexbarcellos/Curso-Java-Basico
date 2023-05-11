6b481b2b53cbb7037fca7661cea8441075dd6ba0
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-05-11 16:59:42 -0300

    ex18 alterado para apresentar o indice da idade.

--- Aula 19 Vetores/Vetores Java/src/ex18_menorMaioIdade.java
+++ Aula 19 Vetores/Vetores Java/src/ex18_menorMaioIdade.java
@@ -5,7 +5,9 @@ public class ex18_menorMaioIdade {
         Scanner scan = new Scanner(System.in);
         int [] vetorIdades = new int[10];
         int max = vetorIdades[0];  
-        int min = vetorIdades[0]; 
+        int maxIndex = 0;
+        int minIndex = 0;
+        int min = Integer.MAX_VALUE; 
         String msg = "\nGrupo das idades:  ";
 
         System.out.println("\nEntre com as idades do grupo: ");
@@ -13,17 +15,19 @@ public class ex18_menorMaioIdade {
             System.out.print("Pessoa " + (i+1) + ":  ");
             vetorIdades[i] = scan.nextInt();
             msg+= vetorIdades[i] + "  ";
-               
-            if (vetorIdades[i] >= max) {
-                max = vetorIdades[i];
-            } else {
+        
+            if (vetorIdades[i] < min) { 
                 min = vetorIdades[i];
-            } 
-        } 
-
+                minIndex = i;
+            }
+            if (vetorIdades[i] > max) { 
+                max = vetorIdades[i];
+                maxIndex = i;
+            }
+        }
         System.out.print(msg + "\n");
-        System.out.println("A menor idade do grupo é " + min);
-        System.out.println("A maior idade do grupo é " + max);
+        System.out.println("A menor idade do grupo é " + min + " - posição " + minIndex);
+        System.out.println("A maior idade do grupo é " + max + " - posição " + maxIndex);
         scan.close();
     }
 }
