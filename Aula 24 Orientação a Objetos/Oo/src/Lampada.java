e5cf0f1a9fc9b1198c7cb31dd21848809488fdae
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-05-29 08:21:35 -0300

    Criado diretório Aula 24 OO e primeiro exercício

---
+++ Aula 24 Orientação a Objetos/Oo/src/Lampada.java
@@ -0,0 +1,11 @@
+public class Lampada {
+    Lampada lamp = new Lampada();
+    String modelo; 
+    int tensao;
+    String cor;
+    String tipoLuz;
+    int potencia;
+    int garantiaMeses;
+    String [] tipos;
+    double preço;
+}
---
+++ Aula 24 Orientação a Objetos/Oo/src/ex01_ClasseLampada.java
@@ -0,0 +1,18 @@
+public class ex01_ClasseLampada {
+    public static void main (String[] args) {
+        Lampada lamp = new Lampada();
+        lamp.cor = "Branca";
+        lamp.modelo = "LUZ10";
+        lamp.garantiaMeses = 12;
+        lamp.potencia = 100;
+        lamp.preço = 12.05;
+        lamp.tensao = 110;
+        lamp.tipoLuz = "Abajour";
+        lamp.tipos = new String[3];
+        lamp.tipos[0] = "Incandescente";
+        lamp.tipos[1] = "Fluorescente";
+        lamp.tipos[2] = "Led";
+
+        System.out.println(lamp.tipos[2] + " de " + lamp.potencia + "w");
+    }
+}
