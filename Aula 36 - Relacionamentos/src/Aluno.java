6eae501b784806a89fb17c7e5be6fbc93e1657c5
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-06-28 22:02:21 -0300

    ex2 em execução

---
+++ Aula 36 - Relacionamentos/src/Aluno.java
@@ -0,0 +1,38 @@
+
+public class Aluno {
+    
+    private String nome;
+    private String matricula;
+    private int[] notas = new int[4];
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(int[] notas) {
+        this.notas = notas;
+    }
+}
