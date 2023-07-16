a63450712f804fb233bdba03fc25d0d5c0051ae0
Alexsandro Barcellos <alexsandro.barcellos@gmail.com>
2023-07-16 19:59:36 -0300

    adicionado título no Ex2 IR

--- Aula 43 Exercícios Herança e Polimorfismo/src/Ex2_ImpostoDeRenda.java
+++ Aula 43 Exercícios Herança e Polimorfismo/src/Ex2_ImpostoDeRenda.java
@@ -1,12 +1,12 @@
 public class Ex2_ImpostoDeRenda {
     public static void main(String[] args) {
         
-        PessoaJuridica pj = new PessoaJuridica("Barcellos ME", "31649678/0001-37", 5600);
-        PessoaFisica pf1 = new PessoaFisica("Alexsandro", "131649678-37", 1000.0);
-        PessoaFisica pf2 = new PessoaFisica("De", "131649678-38", 1500.0);
-        PessoaFisica pf3 = new PessoaFisica("Souza", "131649678-39", 3000.0);
-        PessoaFisica pf4 = new PessoaFisica("Barcellos", "131649678-42", 4000.0);
-        PessoaFisica pf5 = new PessoaFisica("ASB", "131649678-41", 2200.0);
+        PessoaJuridica pj = new PessoaJuridica("Barcellos ME", "31649933/0001-37", 5600);
+        PessoaFisica  pf1 = new PessoaFisica("Alexsandro", "111633654-37", 1000.0);
+        PessoaFisica  pf2 = new PessoaFisica("De", "123456789-38", 1500.0);
+        PessoaFisica  pf3 = new PessoaFisica("Souza", "987654321-39", 3000.0);
+        PessoaFisica  pf4 = new PessoaFisica("Barcellos", "654789321-42", 4000.0);
+        PessoaFisica  pf5 = new PessoaFisica("ASB", "123789456-41", 2200.0);
 
         Contribuinte[] contribuinte = new Contribuinte[6];
             
@@ -17,6 +17,7 @@ public class Ex2_ImpostoDeRenda {
         contribuinte[4] = pf4;
         contribuinte[5] = pf5;
 
+        System.out.println("LISTAGEM DO CONTRIBUINTES, PAGADORES DE IMPOSTOS:\n");
         for (Contribuinte contr : contribuinte) {
             System.out.println(contr.toString());
         }
